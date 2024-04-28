f683e4e3cb05b0fa3b04ea0acfe05af3d1854d9e
petani moden <134819256+walanon-dungun@users.noreply.github.com>
2024-04-28 21:23:47 +0800

    Update proxies.py

--- proxies.py
+++ proxies.py
@@ -7,7 +7,7 @@ test_str = c.text
 a = re.finditer(regex, test_str, re.MULTILINE)
 with open("proxies_list.txt", 'w') as file:
     for i in a:
-       print(i.group(),file=file)
+       print(f'"{i.group()}"', file=file)
         
 d = requests.get("https://free-proxy-list.net/")
 soup = BeautifulSoup(d.content, 'html.parser')
@@ -20,4 +20,4 @@ for j in range(0, len(td_elements), 8):
 with open("proxies_list.txt", "a") as myfile:
     for ip, port in zip(ips, ports):
         proxy = f"{ip}:{port}"
-        print(proxy, file=myfile)
+        print(f'"{proxy}"', file=myfile)
